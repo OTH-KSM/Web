20775350b00c10da49dc38446ba56120bddc394b
OTH-KSM <103659810+OTH-KSM@users.noreply.github.com>
2023-08-10 20:57:01 +0100

    Add files via upload
    active button

---
+++ function.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById("contact").addEventListener("click", function() {
+    const section2 = document.getElementById("social");
+    section2.scrollIntoView({ behavior: "smooth" });
+})
